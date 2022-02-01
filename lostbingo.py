fa4a4c701be4ebb570df20d284aca7ca43afc85e
root <root@DESKTOP-7CL256P.localdomain>
2022-02-01 21:26:11 +0900

    new_ver

--- lostbingo.py
+++ lostbingo.py
@@ -1,16 +1,5 @@
-import tkinter
-
-window=tkinter.Tk()
-window.title("lostbingo")
-window.geometry("1200x650+200+100")
-
-canvas = tkinter.Canvas(width=500,height=600,bg="white")
-canvas.pack()
-for y in range(5):
-    for x in range(5):
-        canvas.create_rectangle(x*100,y*100,x*100+100,y*100+100)
-
-button = tkinter.Button(window, overrelief="solid", width=10, text = '1,1')
-button.pack()
-
-window.mainloop()
+# git status
+# git commit -m "message"
+# git add . /현재 경로의 모든 파일 에드
+# git add 경로/파일이름
+# git checkout 파일이름
--- practice.py
+++ practice.py
@@ -1,5 +1,6 @@
 from gettext import find
 from itertools import count
+import re
 from sre_parse import State
 import tkinter
 from tkinter.constants import COMMAND
@@ -12,7 +13,10 @@ canvas = tkinter.Canvas(width=400,height=400,bg="white")
 canvas.pack()
 x_bingo = []
 y_bingo = []
+x_bingo_count = 0
+y_bingo_count = 0
 check_bnt = []
+cross_bingo = []
 dic = {}
 count = 0
 points = [0,200,200,400,400,200,200,0]
@@ -29,9 +33,13 @@ def find_index(x,y):
 
 def drow_oval(x,y):
     def drow_oval_2():
+        global x_bingo_count
+        global y_bingo_count
+        global count
+        global x_bingo
+        global cross_bingo
         x_bingo_count = 0
         y_bingo_count = 0
-        global count
         if count < 2:
             button1 = dic[(x,y)]
             button1["state"] = 'disabled'
@@ -44,51 +52,72 @@ def drow_oval(x,y):
                 button1 = dic[(check_bnt[1][0]-1,check_bnt[1][1]-1)]
                 button1["state"] = tkinter.NORMAL
             return
-        print(x_bingo)
+
+
+        if y+1 in x_bingo: 
+            drow_oval_left_up(x,y)
+            drow_oval_right_up(x,y)
+            drow_oval_right_down(x,y)
+            drow_oval_left_down(x,y)
+            for i in range(1,6):        
+                x_bingo_count = 0
+                for j in range(len(check_bnt)):
+                    if check_bnt[j][1] == i:
+                        x_bingo_count = x_bingo_count + 1
+                if x_bingo_count == 5:
+                    if i not in x_bingo:
+                        x_bingo.append(i)
+                        print(x_bingo)
+                    for k in range(5):
+                        canvas.create_oval(170-40*(i-1)+40*k,15+40*(i-1)+40*k,230-40*(i-1)+40*k,65+40*(i-1)+40*k,outline='red',width='2')
+                else:
+                    pass
+            return
+
+        if x+1 in y_bingo:  
+            drow_oval_left_up(x,y)
+            drow_oval_right_up(x,y)
+            drow_oval_right_down(x,y)
+            drow_oval_left_down(x,y)
+            for i in range(1,6):        
+                y_bingo_count = 0
+                for j in range(len(check_bnt)):
+                    if check_bnt[j][0] == i:
+                        y_bingo_count = y_bingo_count + 1
+                if y_bingo_count == 5:
+                    if i not in y_bingo:
+                        y_bingo.append(i)
+                    for k in range(5):
+                        canvas.create_oval(170-40*k+40*(i-1),15+40*k+40*(i-1),230-40*k+40*(i-1),65+40*k+40*(i-1),outline='red',width='2')
+                else:
+                    pass
+            return
+        if 1 in cross_bingo:
+            drow_oval_left_up(x,y)
+            drow_oval_right_up(x,y)
+            drow_oval_right_down(x,y)
+            drow_oval_left_down(x,y)
+            return
+        if 2 in cross_bingo:
+            drow_oval_left_up(x,y)
+            drow_oval_right_up(x,y)
+            drow_oval_right_down(x,y)
+            drow_oval_left_down(x,y)
+            return
+
         if [x+1,y+1] in check_bnt:
-            canvas.create_oval(170-40*y+40*x,15+40*y+40*x,230-40*y+40*x,65+40*y+40*x,outline='white',width='2')  #(170,15,230,65) == 1,1 에 그리는 원좌표
+            canvas.create_oval(170-40*y+40*x,15+40*y+40*x,230-40*y+40*x,65+40*y+40*x,outline='white',width='2')
             check_bnt.pop(find_index(x+1,y+1))
         else:
-            canvas.create_oval(170-40*y+40*x,15+40*y+40*x,230-40*y+40*x,65+40*y+40*x,outline='blue',width='2')
+            canvas.create_oval(170-40*y+40*x,15+40*y+40*x,230-40*y+40*x,65+40*y+40*x,outline='blue',width='2')  #(170,15,230,65) == 1,1 에 그리는 원좌표
             check_bnt.append([x+1,y+1])
-        if x-1>=0:
-            print(x+1,y+1)
-            if y+1 in x_bingo:
-                return
-            if [x,y+1] in check_bnt:
-                canvas.create_oval(170-40*y+40*(x-1),15+40*y+40*(x-1),230-40*y+40*(x-1),65+40*y+40*(x-1),outline='white',width='2')
-                check_bnt.pop(find_index(x,y+1))
-            else:
-                canvas.create_oval(170-40*y+40*(x-1),15+40*y+40*(x-1),230-40*y+40*(x-1),65+40*y+40*(x-1),outline='blue',width='2')
-                check_bnt.append([x,y+1])
-        if x+1<5:
-            if y+1 in x_bingo:
-                return
-            if [x+2,y+1] in check_bnt:
-                canvas.create_oval(170-40*y+40*(x+1),15+40*y+40*(x+1),230-40*y+40*(x+1),65+40*y+40*(x+1),outline='white',width='2')
-                check_bnt.pop(find_index(x+2,y+1))
-            else:
-                canvas.create_oval(170-40*y+40*(x+1),15+40*y+40*(x+1),230-40*y+40*(x+1),65+40*y+40*(x+1),outline='blue',width='2')
-                check_bnt.append([x+2,y+1])
-        if y-1>=0:
-            if y+1 in x_bingo:
-                return
-            if [x+1,y] in check_bnt:
-                canvas.create_oval(170-40*(y-1)+40*x,15+40*(y-1)+40*x,230-40*(y-1)+40*x,65+40*(y-1)+40*x,outline='white',width='2')
-                check_bnt.pop(find_index(x+1,y))
-            else:
-                canvas.create_oval(170-40*(y-1)+40*x,15+40*(y-1)+40*x,230-40*(y-1)+40*x,65+40*(y-1)+40*x,outline='blue',width='2')
-                check_bnt.append([x+1,y])
-        if y+1<5:
-            if y+1 in x_bingo:
-                return
-            if [x+1,y+2] in check_bnt:
-                canvas.create_oval(170-40*(y+1)+40*x,15+40*(y+1)+40*x,230-40*(y+1)+40*x,65+40*(y+1)+40*x,outline='white',width='2')
-                check_bnt.pop(find_index(x+1,y+2))
-            else:
-                canvas.create_oval(170-40*(y+1)+40*x,15+40*(y+1)+40*x,230-40*(y+1)+40*x,65+40*(y+1)+40*x,outline='blue',width='2')
-                check_bnt.append([x+1,y+2])
-        for i in range(1,6):        
+            
+        drow_oval_right_up(x,y)
+        drow_oval_left_up(x,y)
+        drow_oval_right_down(x,y)
+        drow_oval_left_down(x,y)
+
+        for i in range(1,6):    # x줄 빙고 확인    
             x_bingo_count = 0
             for j in range(len(check_bnt)):
                 if check_bnt[j][1] == i:
@@ -97,21 +126,104 @@ def drow_oval(x,y):
                 if i not in x_bingo:
                     x_bingo.append(i)
                     for k in range(5):
-                        canvas.create_oval(170-40*y+40*k,15+40*y+40*k,230-40*y+40*k,65+40*y+40*k,outline='red',width='2')
+                        canvas.create_oval(170-40*(i-1)+40*k,15+40*(i-1)+40*k,230-40*(i-1)+40*k,65+40*(i-1)+40*k,outline='red',width='2')
                 else:
                     pass
-        # for i in range(1,6):        
-        #     y_bingo_count = 0
-        #     for j in range(len(check_bnt)):
-        #         if check_bnt[j][0] == i:
-        #             y_bingo_count = y_bingo_count + 1
-        #     if y_bingo_count == 5:
-        #         for k in range(5):
-        #             print(101)
-        #             canvas.create_oval(170-40*y+40*x,15+40*y+40*x,230-40*y+40*x,65+40*y+40*x,outline='red',width='2')
+        for i in range(1,6):    # y줄 빙고확인 
+            y_bingo_count = 0
+            for j in range(len(check_bnt)):
+                if check_bnt[j][0] == i:
+                    y_bingo_count = y_bingo_count + 1
+            if y_bingo_count == 5:
+                if i not in y_bingo:
+                    y_bingo.append(i)
+                    for k in range(5):
+                        canvas.create_oval(170-40*k+40*(i-1),15+40*k+40*(i-1),230-40*k+40*(i-1),65+40*k+40*(i-1),outline='red',width='2')
+                else:
+                    pass   
+
+        # 대각선 빙고 확인 로직 만들기
+        if [1,1] in check_bnt and [2,2] in check_bnt and [3,3] in check_bnt and [4,4] in check_bnt and [5,5] in check_bnt:
+            for i in range(5):
+                canvas.create_oval(170-40*i+40*i,15+40*i+40*i,230-40*i+40*i,65+40*i+40*i,outline='red',width='2')
+                cross_bingo.append(1)
+        if [1,5] in check_bnt and [2,4] in check_bnt and [3,3] in check_bnt and [4,2] in check_bnt and [5,1] in check_bnt:
+            for i in range(1,6):
+                canvas.create_oval(170-40*(i-1)+40*(5-i),15+40*(i-1)+40*(5-i),230-40*(i-1)+40*(5-i),65+40*(i-1)+40*(5-i),outline='red',width='2')
+                cross_bingo.append(2)
+            
                 
-        
     return drow_oval_2
+
+def drow_oval_left_up(x,y):
+    if x-1>=0: #좌상단 위치 3,3 > 2,3
+        if y+1 in x_bingo:
+            return
+        if x in y_bingo:
+            return
+        if 1 in cross_bingo or 2 in cross_bingo:
+            if [x,y+1] in check_bnt:
+                canvas.create_oval(170-40*y+40*(x-1),15+40*y+40*(x-1),230-40*y+40*(x-1),65+40*y+40*(x-1),outline='white',width='2')
+                check_bnt.pop(find_index(x,y+1))
+            else:
+                canvas.create_oval(170-40*y+40*(x-1),15+40*y+40*(x-1),230-40*y+40*(x-1),65+40*y+40*(x-1),outline='blue',width='2')
+                check_bnt.append([x,y+1])
+            return
+        if [x,y+1] in check_bnt:
+            canvas.create_oval(170-40*y+40*(x-1),15+40*y+40*(x-1),230-40*y+40*(x-1),65+40*y+40*(x-1),outline='white',width='2')
+            check_bnt.pop(find_index(x,y+1))
+        else:
+            canvas.create_oval(170-40*y+40*(x-1),15+40*y+40*(x-1),230-40*y+40*(x-1),65+40*y+40*(x-1),outline='blue',width='2')
+            check_bnt.append([x,y+1])
+    else:
+        return
+
+def drow_oval_right_up(x,y):
+    if y-1>=0: #우상단 위치 3,3 > 3,2
+        if y in x_bingo:
+            return
+        if x+1 in y_bingo:
+            return
+        if 1 in cross_bingo:
+            return
+        if [x+1,y] in check_bnt:
+            canvas.create_oval(170-40*(y-1)+40*x,15+40*(y-1)+40*x,230-40*(y-1)+40*x,65+40*(y-1)+40*x,outline='white',width='2')
+            check_bnt.pop(find_index(x+1,y))
+        else:
+            canvas.create_oval(170-40*(y-1)+40*x,15+40*(y-1)+40*x,230-40*(y-1)+40*x,65+40*(y-1)+40*x,outline='blue',width='2')
+            check_bnt.append([x+1,y])
+    else:
+        return
+
+def drow_oval_right_down(x,y):
+    if x+1<5: #우하단 위치 3,3 > 4,3
+        if y+1 in x_bingo:
+            return
+        if x+2 in y_bingo:
+            return
+        if 1 in cross_bingo:
+            return
+        if [x+2,y+1] in check_bnt:
+            canvas.create_oval(170-40*y+40*(x+1),15+40*y+40*(x+1),230-40*y+40*(x+1),65+40*y+40*(x+1),outline='white',width='2')
+            check_bnt.pop(find_index(x+2,y+1))
+        else:
+            canvas.create_oval(170-40*y+40*(x+1),15+40*y+40*(x+1),230-40*y+40*(x+1),65+40*y+40*(x+1),outline='blue',width='2')
+            check_bnt.append([x+2,y+1])
+
+def drow_oval_left_down(x,y):
+    if y+1<5: #좌하단 위치 3,3 > 3,4
+        if y+2 in x_bingo:
+            return
+        if x+1 in y_bingo:
+            return
+        if 1 in cross_bingo:
+            return
+        if [x+1,y+2] in check_bnt:
+            canvas.create_oval(170-40*(y+1)+40*x,15+40*(y+1)+40*x,230-40*(y+1)+40*x,65+40*(y+1)+40*x,outline='white',width='2')
+            check_bnt.pop(find_index(x+1,y+2))
+        else:
+            canvas.create_oval(170-40*(y+1)+40*x,15+40*(y+1)+40*x,230-40*(y+1)+40*x,65+40*(y+1)+40*x,outline='blue',width='2')
+            check_bnt.append([x+1,y+2])
 
 
 for x in range(5):
